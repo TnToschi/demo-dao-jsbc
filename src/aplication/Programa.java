6cb6cbc4d6ff77710deed758416a003da37698d5
TnToschi <tulio_toschi@hotmail.com>
2020-04-11 09:34:29 -0300

    Delete implementation

--- src/aplication/Programa.java
+++ src/aplication/Programa.java
@@ -2,6 +2,7 @@ package aplication;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,38 +13,47 @@ public class Programa {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		System.out.println("=== Teste 1: seller findById===");
+		System.out.println("=== Teste 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);	
 		
-		System.out.println("\n=== Teste 2: seller findByDepartment===");
+		System.out.println("\n=== Teste 2: seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDeparment(department);
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== Teste 3: seller findAll===");
+		System.out.println("\n=== Teste 3: seller findAll ===");
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n=== Teste 4: seller insert===");
+		System.out.println("\n=== Teste 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
 		
-		System.out.println("\n=== Teste 5: seller UpDate===");
+		System.out.println("\n=== Teste 5: seller UpDate ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Wayne");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
 		
+		System.out.println("\n=== Teste 6: seller Delete ===");
+		System.out.println("Enter id for delete teste: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
 		
+		
+		sc.close();
 	}
 
 }
